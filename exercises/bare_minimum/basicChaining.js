224739bf70ad13a4075dadadd4e10e2ccf20197d
darienpoon <darienpoon1@gmail.com>
2023-06-06 16:56:22 -0700

    completed basic chaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,12 +10,39 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var { getGitHubProfileAsync } = require('./promisification');
+Promise.promisifyAll(fs);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // return a "promise"
+  // 1. invoke fs.readFile to return first line of file
+  return fs.readFileAsync(readFilePath).then((data)=> {
+    if (!data) {
+      throw new Error('File not read');
+    } else {
+      data = data.toString().split('\n');
+      return data[0];
+    }
+  })
+    .then ((username) => {
+      return getGitHubProfileAsync(username);
+    })
+    .then ((userData) => {
+      if (!userData) {
+        throw new Error ('User info not found');
+      } else {
+        return fs.writeFileAsync (writeFilePath, JSON.stringify(userData));
+      }
+    });
 };
+
+
+
+// 2. invoke getGitHubProfileAsync and pass in username return from last step, returns user's profile?
+// 3. invokes fs.writeFile to "writeFilePath" and write the profile return from previous step
+
+
+
 
 // Export these functions so we can test them
 module.exports = {
